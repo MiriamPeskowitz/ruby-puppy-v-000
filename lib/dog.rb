964256b20191a8f8bea249c4c72ab1eebc229e71
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-28 20:14:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
 @@all = []
-attr_accessor :name 
+attr_accessor :name
 
   def initialize(name)
     @name = name
     @@all<<self
-    # why is this not @@all<<name?? 
+    # why is this not @@all<<name??
   end
 
   def self.all
@@ -18,30 +18,8 @@ attr_accessor :name
     @@all.clear
   end
 end
-    
- 
 
 
 
 
-  
-  @@all = []
 
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all.each do |puppy|
-      puts puppy.name
-    end
-  end
-
-  def self.clear_all
-    @@all.clear
-  end
-
-end
